4253063c4d1964dbc3ac8b046005edaf9b0dc367
Denny <kleinerumbero@gmail.com>
2025-10-01 19:22:52 +0200

    includet script in html and js scricpt for ingredients

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
-function leseZahlAusFeld(){
-    let inputElement = document.getElementById();
 
-    let valueInputElement = inputElement.value;
+const input = document.getElementById("personen");
+const zutaten = document.querySelectorAll("#zutaten-tabelle td");
 
-    let recipeAmount = parseInt(valueInputElement);
-
-    if (isNaN(recipeAmount)){
-        alert("Bitte gültige Zahl eingeben!");
-
-    }
-
-
-    
+// Funktion: Zutaten neu berechnen
+function updateZutaten() {
+    let personen = parseInt(input.value); // eingegebene Zahl
+    zutaten.forEach(td => {
+        // Basiswert aus Attribut lesen (z. B. 500 für Kartoffeln)
+        let basis = parseInt(td.getAttribute("data-basis"));
+        // neue Menge = Basis * Personen
+        let neueMenge = basis * personen;
+        console.log(neueMenge);
+        // Text ersetzen (nur die Zahl am Anfang)
+        td.textContent = td.textContent.replace(/^\d+/, neueMenge);
+    });
 }
